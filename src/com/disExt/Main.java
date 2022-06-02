73142ed3951f31803b03d327e9b01157fc96e98c
Veronica <veronicaciancia@gmail.com>
2022-06-02 19:29:45 +0200

    Correccion Menu

--- src/com/disExt/Main.java
+++ src/com/disExt/Main.java
@@ -14,14 +14,12 @@ public class Main {
         int opcion = 0; //inicializamos la variable para elegir que opcion del menu
         Boolean salir = false; //inicializamos salir como false, para que no salga del menu
 
-        try(Scanner scanner = new Scanner(System.in)){
             while (salir == false){
                 System.out.println("\n\n       MENU OPCIONES INICIAL     ");
                 System.out.println("\n 1- BUSCAR POR FECHA  ");
                 System.out.println("\n 2- MOSTRAR ENTRADAS 1000<x<10000");
                 System.out.println("\n 3- EXPORTAR NUEVO PDF ");
                 System.out.println("\n 4  SALIR");
-                opcion = scanner.nextInt();
 
                 switch (opcion){
                     case 1:
@@ -39,9 +37,7 @@ public class Main {
                         System.out.println("error");
                 }
             }
-        } catch (ParseException e) {
-            e.printStackTrace();
         }
-        
+
     }
 }

aaca6b4770d65eec4d8f094805016843a08b6948
migueels <salasrmcf1997@gmail.com>
2022-06-02 20:18:46 +0200

    ultimo metodo (exportar CSV)

--- src/com/disExt/programMethods.java
+++ src/com/disExt/programMethods.java
@@ -1,6 +1,11 @@
 package com.disExt;
 
+
+import au.com.bytecode.opencsv.CSVWriter;
+
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -55,4 +60,38 @@ public class programMethods {
         System.out.println("\n\nCon un importe total de:  " + importeTotal);
 
     }
+
+    //funcion exportar un nuevo CSV
+    public static void exportCSV(String filename,Tarjeta tarjetadata[]) throws IOException{
+        //CardDatos[] cardDatos = new CardDatos[18025];
+        String[] data = new String[tarjetadata.length];
+        int i= 0;
+        //String csv = "salida.csv";
+        String[] headers = {"Tittles 2"};
+        CSVWriter newCsv = new CSVWriter(new FileWriter(filename), '\n');
+        //CSVWriter newCsv = new CSVWriter(new FileWriter(filename));
+
+        newCsv.writeNext(headers);
+
+
+        for(int j = 0; j < tarjetadata.length; j++){
+            if("credit".equalsIgnoreCase(tarjetadata[j].getTitles2())){
+
+                data[i] = String.valueOf(tarjetadata[j].getValue());
+                i++;
+            }
+        }
+
+        newCsv.writeNext(data);
+        newCsv.close();
+
+        File archivo = new File("salida.csv");
+        if (!archivo.exists()) { // si no existe
+            System.out.println("El archivo indicado no existe");
+        } else {
+            System.out.println("\nSe creo el archivo salida.csv correctamente");
+        }
+
+    }
+
 }
